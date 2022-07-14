3ee8185c6801cddff6a7e082b316b0a4e5a3a1dc
ywanzhou <ywanzhou@foxmail.com>
2022-07-14 15:24:18 +0800

    install naive ui

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import './index.css'
+// 通用字体
+import 'vfonts/Lato.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: 'src/type/components.d.ts',
+    }),
+  ],
 })
