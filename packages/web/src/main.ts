b896a19724e40097e6a4a67ddee78f5f654f02ea
ywanzhou <ywanzhou@foxmail.com>
2022-07-14 21:45:37 +0800

    安装daisyUI

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import './index.css'
 // 通用字体
 import 'vfonts/Lato.css'
--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+// import Components from 'unplugin-vue-components/vite'
+// import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    Components({
-      resolvers: [NaiveUiResolver()],
-      dts: 'src/type/components.d.ts',
-    }),
+    // Components({
+    //   resolvers: [NaiveUiResolver()],
+    //   dts: 'src/type/components.d.ts',
+    // }),
   ],
 })
