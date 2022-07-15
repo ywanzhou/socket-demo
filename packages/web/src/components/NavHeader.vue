2c3d093964b239aa73e4d3efe7ba4eda0ef80c1a
ywanzhou <ywanzhou@foxmail.com>
2022-07-15 22:33:37 +0800

    拆分头部栏组件

---
+++ packages/web/src/components/NavHeader.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Props {
+  groupName: string
+  personNumber: number
+}
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <!-- 顶部栏 -->
+  <div class="navbar text-primary-content rounded-box space-x-1 h-16">
+    <div class="flex-1">
+      <a class="normal-case text-xl pl-4"
+        >{{ props.groupName }}({{ props.personNumber }})</a
+      >
+    </div>
+    <div class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          class="inline-block w-5 h-5 stroke-current"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
