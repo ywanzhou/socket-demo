2226e6f1195048e0fa504140fec182690b97058c
ywanzhou <ywanzhou@foxmail.com>
2022-07-16 11:11:33 +0800

    定义头像列表

---
+++ packages/web/src/assets/avatar.ts
@@ -0,0 +1,14 @@
+// 头像来自网络 侵删
+export default [
+  'https://img2.woyaogexing.com/2022/07/14/ee6caa770949cdf0!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/14/ecaee2b5db287cb7!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/13/2590ad3b1059a862!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/06/db509263d6184ba6!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/06/6d7b679031b280a0!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/06/ee83d16254828a3e!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/06/9789984ddc6a0837!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/06/375a04db8b775a72!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/06/ee83d16254828a3e!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/06/9789984ddc6a0837!400x400.jpg',
+  'https://img2.woyaogexing.com/2022/07/05/51c7c0b9a3514dac!400x400.jpg',
+]
