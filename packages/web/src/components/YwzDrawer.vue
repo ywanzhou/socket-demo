729137fac02ea5c76cf3722546f9c274d7a280df
Zhou-Zhonghua <pacpc@qq.com>
2022-08-06 20:54:44 +0800

    编写抽屉组件

---
+++ packages/web/src/components/YwzDrawer.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+
+interface Props {
+  modelValue: boolean
+}
+const props = defineProps<Props>()
+const emit = defineEmits(['update:modelValue'])
+const aniShow = ref(false)
+const show = computed({
+  get() {
+    aniShow.value = props.modelValue
+    return props.modelValue
+  },
+  set(v) {
+    emitUpdate(v)
+  },
+})
+const emitUpdate = (v: boolean, time = 300) => {
+  aniShow.value = v
+  window.setTimeout(() => {
+    emit('update:modelValue', v)
+  }, time)
+}
+</script>
+<template>
+  <div class="ywz-drawer h-screen w-screen fixed z-10 top-0" v-show="show">
+    <div
+      class="drawer-overlay absolute inset-0 z-0"
+      @click="emitUpdate(false)"
+    ></div>
+    <Transition>
+      <div
+        v-show="aniShow"
+        class="drawer-content absolute right-0 top-0 cursor-default h-screen bg-base-100"
+      >
+        <slot></slot>
+      </div>
+    </Transition>
+  </div>
+</template>
+<style scoped>
+.drawer-overlay {
+  --tw-bg-opacity: 0.4;
+  opacity: 0.999999;
+  cursor: pointer;
+  background-color: hsl(var(--nf, var(--n)) / var(--tw-bg-opacity));
+  transition-property: all;
+  transition-duration: 0.3s;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+/* 下面我们会解释这些 class 是做什么的 */
+.v-enter-active,
+.v-leave-active {
+  transition-property: all;
+  transition-duration: 0.3s;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.v-enter-from,
+.v-leave-to {
+  transform: translateX(100%);
+}
+</style>
